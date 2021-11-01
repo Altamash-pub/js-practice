7b06388035091af55ef86f06963b42a01bfd2054
Altamash-pub <90692333+Altamash-pub@users.noreply.github.com>
2021-11-01 20:48:26 +0530

    basic js

---
+++ DataTypes.js
@@ -0,0 +1,48 @@
+/* Dta types in Java Script*/
+
+ /* there are two types 
+  1. Primitive data type: { numbers, String, undefined, null, boolean , symbol }
+  1. Reference data type:{ Objects and arrays } */
+
+//Number
+var num1=455;
+var num2=405;
+
+//String
+
+var str1='this is string'
+var str2="this is also string"
+var sumstr=  str1+ " " +str2
+
+//Objects
+
+var marks={
+    naruto: 34,
+    lee:33,
+    naji:75
+}
+
+//Booleans// "thats are TRUE and FALSE"
+
+var a = true;
+var b = false ;
+
+// Undefined and Null//
+
+// undefined means => value which is not present in a function or that means  undefined //
+
+var und;
+
+
+// Null means kuch nahi yani maai ne hi asign nahi kya is me //
+var n = null;
+ 
+//Array//
+
+var arr = [ 1,2,"bablu",4,5]
+console.log (arr)
+
+let selectedColors = ['red','blue'];
+selectedColors[2]= 5;
+console.log(selectedColors.length);
+
---
+++ Operators.js
@@ -0,0 +1,43 @@
+//OPerators//
+
+//Arethematic operators//
+
+var a= 100;
+var b= 10;
+
+// console.log("The value of a + b", a+b)
+// console.log("The value of a - b", a-b)
+// console.log("The value of a * b", a*b)
+// console.log("The value of a / b", a/b)
+
+//Assignment OPerators//
+
+var c = b;
+c+=2;      // c = c+2
+c-=2;      // c = c-2
+c*=2;      // c = c*2
+c/=2;      // c = c/2
+// console.log(c)
+
+// Comparision operators//
+
+var x = 34;
+var y = 36;
+// console.log(x==y)
+// console.log(x>=y)
+// console.log(x<=y)
+
+// logical && and || //
+
+//&& and operator me dono bhi condition true padta hai//
+//   || or operator me 1 bhi condition sahi hai to matlab sab sahi hai means true hai//
+
+// console.log( true && true)
+// console.log( false && true)
+// console.log( true || false)
+// console.log( false && false)
+
+//  (!) Logical not => means sach ku jhot or jhot ku sach bata hai // => matlab ulta kardeta
+
+console.log(!false)
+console.log(!true)
