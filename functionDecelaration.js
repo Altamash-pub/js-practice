041b396eb7bddaf6e872d47f4691c12a1ca1301b
Altamash-pub <90692333+Altamash-pub@users.noreply.github.com>
2021-11-02 11:54:59 +0530

    decelarationoffunction

---
+++ functionDecelaration.js
@@ -0,0 +1,15 @@
+//performing a task
+
+function greet (name,lastName){
+    console.log('Hello ' + name + ' '+ lastName);
+}
+greet('altamash');
+
+// calculating a value
+
+// function square (number){
+// return number*number;
+// }
+// console.log(square(2));
+
+
